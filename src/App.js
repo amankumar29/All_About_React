58e8c0a0be3b115995a5a26eed5c57b3449e280a
codewithhappy <mannukumaraman@gmail.com>
2021-11-24 17:38:31 +0530

    React commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Fcomponent from "./Component/Fcomponent";
+// import Ccomponent from "./Component/Ccomponent";
+import Comp3 from "./Component/Comp3";
+import Func1 from "./Component/Func1";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Fcomponent/>
+  <Comp3/>
+  <Func1/>
+  {/* <Ccomponent/> */}
+  </>
   );
 }
 
